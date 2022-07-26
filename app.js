a8bf3cf2df90970ec298fecb07713b004322e319
dennisnyarko <dennis_nyarko@outlook.com>
2022-07-26 14:17:48 +0000

    add switch statement

--- app.js
+++ app.js
@@ -12,14 +12,32 @@ app.get("/", function(req, res){
     var currentDay = today.getDay();
     var day = "";
 
-    if (currentDay === 6 || currentDay === 0){
-        day = "Weekend";
-        
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+        case 6:
+            day = "Saturday";
+            break;
+        default:
+            console.log("Error: Current day is equal to: " + currentDay);
     }
-    else {
-        day = "Weekday";
         
-    }
+    
     res.render("list", {kindOfDay: day});
 
 });
@@ -27,4 +45,5 @@ app.get("/", function(req, res){
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
 });
+
 
