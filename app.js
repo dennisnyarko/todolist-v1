1c1b6311916a446491fd50fcf09eec486fe0cb56
dennisnyarko <dennis_nyarko@outlook.com>
2022-07-29 17:31:36 +0000

    update to add list

--- app.js
+++ app.js
@@ -4,46 +4,42 @@ const request = require('request');
 
 const app = express();
 
+var items = [];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
 
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-        case 6:
-            day = "Saturday";
-            break;
-        default:
-            console.log("Error: Current day is equal to: " + currentDay);
-    }
-        
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var day = today.toLocaleDateString("en-US", options);
+   
     
-    res.render("list", {kindOfDay: day});
+    res.render("list", {kindOfDay: day, newListItem: items});
 
 });
+
+app.post("/", function(req, res){
+    var item =  req.body.newItem;
+
+    items.push(item);
+
+    res.redirect("/");  
+});
+
+
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
 });
+
 
 
