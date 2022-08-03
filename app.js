6dd8ca24f428192cbf36a573923e8ce5bc9086d4
dennisnyarko <dennis_nyarko@outlook.com>
2022-08-03 23:51:57 +0000

    add work page

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const request = require('request');
 
 const app = express();
 
-let items = [];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -24,7 +25,7 @@ app.get("/", function(req, res){
     let day = today.toLocaleDateString("en-US", options);
    
     
-    res.render("list", {kindOfDay: day, newListItems: items});
+    res.render("list", {listTitle: day, newListItems: items});
 
 });
 
@@ -36,6 +37,15 @@ app.post("/", function(req, res){
     res.redirect("/");  
 });
 
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.post("/work", function(req, res){
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work")
+})
 
 
 app.listen(3000, function(){
