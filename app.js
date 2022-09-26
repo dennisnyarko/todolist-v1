d993f5308d14aa422516ed431cc2946e4b92d33c
dennisnyarko <dennis_nyarko@outlook.com>
2022-09-26 14:40:13 +0000

    restore all changes

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require("mongoose");
-const date = require(__dirname + "/date.js");
 const request = require('request');
 
 const app = express();
+
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
-
-const itemsSchema = {
-    name: String
-};
-
-const Item = mongoose.model("Item", itemsSchema);
 
 app.get("/", function(req, res){
 
